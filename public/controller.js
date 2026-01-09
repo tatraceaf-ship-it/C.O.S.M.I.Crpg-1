93fda8d000e4090042665965e69184aac2f5684a
Tatrace <tatirookami@gmail.com>
2026-01-08 23:13:00 -0300

    Update controller.js

--- public/controller.js
+++ public/controller.js
@@ -1,18 +1,16 @@
 const socket = io();
 
+const HUD_ID = "leafone";
+
 function aplicarHUD() {
   socket.emit("updateHud", {
-    id: "leafone",
-
+    id: HUD_ID,
     name: nome.value,
     level: nivel.value,
-
     vidaAtual: Number(vidaAtual.value),
     vidaMax: Number(vidaMax.value),
-
     manaAtual: Number(manaAtual.value),
     manaMax: Number(manaMax.value),
-
     showVidaBar: true,
     dice: { rolls: [] }
   });
@@ -20,9 +18,8 @@ function aplicarHUD() {
 
 function rolarDado() {
   const r = Math.floor(Math.random() * 20) + 1;
-
   socket.emit("updateHud", {
-    id: "leafone",
+    id: HUD_ID,
     dice: { rolls: [r] }
   });
 }
