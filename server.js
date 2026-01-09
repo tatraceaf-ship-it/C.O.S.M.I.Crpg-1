4740d76deec3a5c558e6374b3ce64908c2ee438a
Tatrace <tatirookami@gmail.com>
2026-01-09 00:06:34 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,27 +3,27 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-// arquivos estáticos
 app.use(express.static("public"));
 
-// ROTA RAIZ -> LOGIN
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/login.html");
+  res.redirect("/controller.html");
 });
 
 io.on("connection", socket => {
-  socket.on("join", id => {
-    socket.join(id);
+  console.log("Socket conectado:", socket.id);
+
+  socket.on("join", hudId => {
+    console.log("Entrou no HUD:", hudId);
+    socket.join(hudId);
   });
 
   socket.on("updateHud", data => {
+    console.log("Update HUD:", data.id);
     io.to(data.id).emit("hudUpdate", data);
   });
 });
-
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
   console.log("Servidor rodando na porta", PORT);
 });
-
