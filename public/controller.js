a644268dbae91334210dc3287d0815080f7f330f
Tatrace <tatirookami@gmail.com>
2026-01-08 21:12:56 -0300

    Update controller.js

--- public/controller.js
+++ public/controller.js
@@ -1,38 +1,23 @@
 const socket = io();
 
-function getValue(id) {
-  return Number(document.getElementById(id).value);
+const ID = new URLSearchParams(location.search).get("id") || "default";
+
+function enviarEstado() {
+  const state = {
+    vidaAtual: Number(document.getElementById("vidaAtual").value),
+    vidaMax: Number(document.getElementById("vidaMax").value),
+    manaAtual: Number(document.getElementById("manaAtual").value),
+    manaMax: Number(document.getElementById("manaMax").value),
+    nivel: Number(document.getElementById("nivel").value),
+    dado: window.lastRoll || null
+  };
+
+  socket.emit("controller:update", { id: ID, data: state });
 }
 
-document.getElementById("apply").onclick = () => {
-  socket.emit("state:update", {
-    vida: {
-      atual: getValue("vidaAtual"),
-      max: getValue("vidaMax")
-    },
-    mana: {
-      atual: getValue("manaAtual"),
-      max: getValue("manaMax")
-    }
-  });
+document.getElementById("aplicar").onclick = enviarEstado;
+document.getElementById("rolar").onclick = () => {
+  window.lastRoll = Math.floor(Math.random() * 20) + 1;
+  enviarEstado();
 };
 
-document.getElementById("roll").onclick = () => {
-  const faces = Number(document.getElementById("dice").value);
-  const qty = Number(document.getElementById("qty").value);
-
-  const results = Array.from({ length: qty }, () =>
-    Math.floor(Math.random() * faces) + 1
-  );
-
-  socket.emit("state:update", {
-    dado: {
-      faces,
-      resultados: results
-    }
-  });
-};
-
-document.getElementById("toggleVida").onclick = () => {
-  socket.emit("state:update", { showVidaBar: false });
-};
