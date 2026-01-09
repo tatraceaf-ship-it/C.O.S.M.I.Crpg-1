e7bf28ca230b0ba406c84d54b4d328c3996a779e
Tatrace <tatirookami@gmail.com>
2026-01-08 21:55:24 -0300

    Update shared-socket.js

--- public/shared-socket.js
+++ public/shared-socket.js
@@ -1,32 +1,19 @@
-// shared-socket.js
 const socket = io();
 
-const DEFAULT_STATE = {
-  nome: "Leafone",
-  nivel: 6,
-  vida: { atual: 36, max: 100 },
-  mana: { atual: 15, max: 15 },
-  dado: null
-};
-
-let state = JSON.parse(localStorage.getItem("HUD_STATE")) || DEFAULT_STATE;
-
-function saveState() {
-  localStorage.setItem("HUD_STATE", JSON.stringify(state));
+function getHudId() {
+  const params = new URLSearchParams(window.location.search);
+  return params.get("id") || "default";
 }
 
-function updateState(patch) {
-  state = { ...state, ...patch };
-  saveState();
-  socket.emit("state:update", state);
+// Controller envia
+function sendHudUpdate(data) {
+  socket.emit("hud:update", {
+    id: getHudId(),
+    data
+  });
 }
 
-socket.on("state:sync", (newState) => {
-  state = newState;
-  saveState();
-  window.dispatchEvent(new CustomEvent("state:changed", { detail: state }));
-});
-
-socket.emit("state:sync");
-
-export { state, updateState };
+// HUD escuta
+function listenHudUpdates(callback) {
+  socket.on(`hud:${getHudId()}`, callback);
+}
